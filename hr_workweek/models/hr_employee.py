25b0e23897ec6c46920c6356c8517db0b6ec7453
Gerloav <german@domatix.com>
2025-03-26 15:33:40 +0000

    Migración 17.0 post modificaciones horas facturadas y eficiencia

--- hr_workweek/models/hr_employee.py
+++ hr_workweek/models/hr_employee.py
@@ -37,15 +37,81 @@ class HrEmployee(models.Model):
         string="Compensations", compute="_compute_compensation_count"
     )
 
-    current_workweek = fields.Many2one(comodel_name="hr.workweek")
+    current_workweek = fields.Many2one(
+        comodel_name="hr.workweek"
+    )
+
+    analytic_line_ids = fields.One2many(
+        'account.analytic.line',
+        'employee_id',
+        string="Timesheets",
+        help="All timesheet entries linked to this employee"
+    )
+
+    total_hours_worked = fields.Float(
+        string="Total Worked",
+        compute="_compute_efficiency",
+        store=True,
+        help="Sum of all unit_amount from timesheets"
+    )
+
+    total_hours_invoiced = fields.Float(
+        string="Total Invoiced",
+        compute="_compute_efficiency",
+        store=True,
+        help="Sum of all unit_amount_invoiced from timesheets"
+    )
+
+    work_efficiency = fields.Float(
+        string="Billing Efficiency",
+        compute="_compute_efficiency",
+        store=True,
+        help="Percentage of worked hours that are billable (unit_amount_invoiced / unit_amount * 100)"
+    )
+
+    invoiced_hours_start_date_str = fields.Date(
+        compute="_compute_invoiced_hours_start_date_str",
+        store=False,
+        string="Invoiced Hours Start Date"
+    )
+
+    def _compute_invoiced_hours_start_date_str(self):
+        for employee in self:
+            ir_config = self.env["ir.config_parameter"].sudo()
+            start_date = ir_config.get_param(
+                "res.config.settings.invoiced_hours_start_date", 
+                default=""
+            )
+            employee.invoiced_hours_start_date_str = start_date if start_date else ""
+
+    @api.depends('analytic_line_ids.unit_amount', 'analytic_line_ids.unit_amount_invoiced')
+    def _compute_efficiency(self):
+        ir_config = self.env["ir.config_parameter"].sudo()
+        start_date_str = ir_config.get_param(
+            "res.config.settings.invoiced_hours_start_date",
+            default=""
+        )
+        start_date = fields.Date.to_date(start_date_str) if start_date_str else ""
+        domain = [('employee_id', 'in', self.ids), ('project_id', '!=', False)]
+        if start_date:
+            domain.append(('date', '>=', start_date))
+        timesheets = self.env['account.analytic.line'].search(domain)
+        for employee in self:
+            emp_timesheets = timesheets.filtered(lambda t: t.employee_id == employee)
+            total_worked = sum(emp_timesheets.mapped('unit_amount'))
+            total_invoiced = sum(emp_timesheets.mapped('unit_amount_invoiced'))
+            
+            employee.total_hours_worked = total_worked
+            employee.total_hours_invoiced = total_invoiced
+            employee.work_efficiency = min(
+                (total_invoiced / total_worked * 100, 100) if total_worked > 0 else 0.0
+            )
 
     def _compute_workweek_ids_count(self):
         for record in self:
             record.workweek_ids_count = len(record.hr_workweek_ids)
 
-    @api.depends(
-        "hr_workweek_ids.hours_difference", "hr_workweek_ids.hours_compensated"
-    )
+    @api.depends("hr_workweek_ids.hours_difference", "hr_workweek_ids.hours_compensated")
     def _compute_hours_difference(self):
         for record in self:
             hours_difference = sum(record.hr_workweek_ids.mapped("hours_difference"))
