aa9caefef8bf1087198eb14f1845d3047b437d1a
Your Name <you@example.com>
2026-06-08 12:13:09 +0000

    fix

--- hr_workweek/models/__init__.py
+++ hr_workweek/models/__init__.py
@@ -4,5 +4,6 @@ from . import project_task
 # from . import hr_holidays_public
 from . import hr_leave
 from . import hr_employee
+from . import hr_employee_public
 from . import hr_workweek
 from . import res_config_settings
